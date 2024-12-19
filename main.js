0b99ee2dac89f77147e2d5972288712f37c4b7bc
dsad233 <ksl1421@naver.com>
2024-12-20 01:43:23 +0900

    Fix : 오류 수정

--- main.js
+++ main.js
@@ -13,17 +13,16 @@ app.listen(port, () => {
     console.log(port, "서버로 실행 중.");
 });
 
-const apiKey = process.env.NVD_API_KEY;
-const cveId = "CVE-2024-12345";
-const cveApiUrl = `https://services.nvd.nist.gov/rest/json/cves/2.0/${cveId}?apiKey=${apiKey}`;
+// const apiKey = process.env.NVD_API_KEY;
 
-app.get('/', async (req, res) => {
+app.get('/:cveId', async (req, res) => {
     try {
+        const { cveId } = req.params;
+        const cveApiUrl = `https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=${cveId}`;
         const data = await fetch(cveApiUrl);
-        console.log(data);
-        // const jsonChange = JSON.parse(data);
+        const jsonChange = await data.json();
 
-        return res.status(200).json({ data : data });
+        return res.status(200).json(jsonChange);
     } catch (err){
         console.log(err);
         return res.status(500).json({ message : "error" });
