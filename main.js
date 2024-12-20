845a8281fe7a6a08934a36be0e5623fa75d0c435
dsad233 <ksl1421@naver.com>
2024-12-20 20:38:58 +0900

    Fix : 키워드를 통한 엑셀 다운로드 기능 구현

--- main.js
+++ main.js
@@ -65,8 +65,8 @@ app.get('/excel/:cveId', async (req, res) => {
                 worksheet.columns = [
                     { header : "취약점 아이디", key : "cve_Id", width : 20 },
                     { header : "취약점 제공사", key : "cve_source", width : 20 },
-                    { header : "취약점 릴리즈", key : "cve_pushed", width : 20 },
-                    { header : "취약점 최종 수정", key : "cve_lastModified", width : 20 },
+                    { header : "취약점 릴리즈 날짜", key : "cve_pushed", width : 20 },
+                    { header : "취약점 최종 수정 날짜", key : "cve_lastModified", width : 20 },
                     { header : "취약점 상태", key : "cve_status", width : 20 }
                 ];
 
@@ -93,7 +93,7 @@ app.get('/excel/:cveId', async (req, res) => {
                 );
                 res.setHeader(
                   "Content-Disposition",
-                  "attachment; filename=" + currentDayFormat + "_" + "excelfile.xlsx"
+                  "attachment; filename=" + "cveIdSearch" + currentDayFormat + "_" + "excelfile.xlsx"
                 );
 
                 // 다운로드
@@ -130,6 +130,84 @@ app.get('/search/:keyword', async (req, res) => {
 
                 const sliceData = jsonChange.vulnerabilities.slice(0, 10);
                 return res.status(200).json(sliceData);
+            } catch (err) {
+                console.log(err);
+                return res.status(500).json({ message: 'JSON으로 파싱 실패' });
+            }
+        } else {
+            return res.status(data.status).json({ message: 'CVE API 요청 실패' });
+        }
+    } catch (err){
+        console.log(err);
+        return res.status(500).json({ message : "서버 에러 발생" });
+    }
+});
+
+
+// keyword를 통한 엑셀 다운로드
+app.get('/search/excel/:keyword', async (req, res) => {
+    try {
+        const { keyword } = req.params;
+        const cveApiUrl = `https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=${keyword}`;
+        const data = await fetch(cveApiUrl);
+        
+        if (!data) {
+            return res.status(404).json({ message: 'CVE 데이터가 존재하지 않습니다.' });
+        }
+
+        const workbook = new ExcelJS.Workbook();
+        
+        if(data.ok){
+            try {
+                const jsonChange = await data.json(); 
+
+                const worksheet = workbook.addWorksheet("CVE Data");
+                worksheet.columns = [
+                    { header : "취약점 아이디", key : "cve_Id", width : 20 },
+                    { header : "취약점 제공사", key : "cve_source", width : 20 },
+                    { header : "취약점 릴리즈 날짜", key : "cve_pushed", width : 20 },
+                    { header : "취약점 최종 수정 날짜", key : "cve_lastModified", width : 20 },
+                    { header : "취약점 상태", key : "cve_status", width : 20 }
+                ];
+
+                const cveData = [];
+                // 검색한 10개의 데이터 조회
+                const sliceData = jsonChange.vulnerabilities.slice(0, 10);
+
+                for(let i = 0; i < sliceData.length; i++){
+                    cveData.push({
+                        cve_Id : sliceData[i].cve.id,
+                        cve_source : sliceData[i].cve.sourceIdentifier,
+                        cve_pushed : sliceData[i].cve.published,
+                        cve_lastModified : sliceData[i].cve.lastModified,
+                        cve_status : sliceData[i].cve.vulnStatus,
+                    });
+                }
+
+                worksheet.addRows(cveData);
+
+                const currentDate = new Date();
+                const currentDayFormat =
+                currentDate.getFullYear() +
+                "-" +
+                (currentDate.getMonth() + 1) +
+                "-" +
+                currentDate.getDate();
+
+                res.header("Access-Control-Expose-Headers", "Content-Disposition");
+                res.setHeader(
+                  "Content-Type",
+                  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                );
+                res.setHeader(
+                  "Content-Disposition",
+                  "attachment; filename=" + "keywordSearch" + currentDayFormat + "_" + "excelfile.xlsx"
+                );
+
+                // 다운로드
+                await workbook.xlsx.write(res);
+                res.end();
+
             } catch (err) {
                 console.log(err);
                 return res.status(500).json({ message: 'JSON으로 파싱 실패' });
